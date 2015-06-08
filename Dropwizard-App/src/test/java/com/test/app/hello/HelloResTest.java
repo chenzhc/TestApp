257cbf6c9ff806a800b54f723e241e7d5ad1f224
zc <80132938@qq.com>
2015-06-08 15:47:50 +0800

    add: WebTarget test

--- Dropwizard-App/src/test/java/com/test/app/hello/HelloResTest.java
+++ Dropwizard-App/src/test/java/com/test/app/hello/HelloResTest.java
@@ -1,11 +1,15 @@
 package com.test.app.hello;
 
+import com.sun.net.httpserver.HttpServer;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -27,5 +31,24 @@ public class HelloResTest {
         String str2 = resource.request(MediaType.APPLICATION_JSON)
                 .get(String.class);
         System.out.println(str2);
+    }
+
+    private HttpServer server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() throws Exception {
+        Client c = ClientBuilder.newClient();
+        target = c.target("http://localhost:8080");
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() {
+        String responseMsg = target.path("hello-world").path("getHello")
+                .request().get(String.class);
+        assertEquals("Hello world!", responseMsg);
     }
 }
